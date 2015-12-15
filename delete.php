4f39bb5690f5270eef7330c4c9565e51a9640c4c
Eduardo Pérez <eperezf@pisapapeles.net>
2015-12-14 21:55:54 -0300

    Deleter and lister ready

---
+++ delete.php
@@ -0,0 +1,36 @@
+<?php 
+
+$id = $_GET["id"];
+echo "<p>ID ha sido seteada: " . $id . "</p>";
+
+include('config.php');
+echo "<p>config.php incluído.</p>";
+
+$queryB = "DELETE FROM `Telefonos_Bandas` WHERE `idTelefonos` = " . $id;
+echo "<p>Query de bandas creado: " . $queryB . "</p>";
+
+$queryT = "DELETE FROM `Telefonos` WHERE `idTelefonos` = " . $id;
+echo "<p>Query de teléfono creado: " . $queryT . "</p>";
+
+$resultB = mysqli_query($conn, $queryB);
+echo "<p>Query de bandas realizado<p>";
+
+if (!$resultB) {
+	echo "<p>ERROR: NO SE HAN ELIMINADO LAS BANDAS DEL TELÉFONO</p>";
+}
+else {
+	echo "<p>LAS BANDAS DEL TELEFONO HAN SIDO ELIMINADAS</p>";
+}
+
+$resultT = mysqli_query($conn, $queryT);
+echo "<p>Query de bandas realizado<p>";
+
+if (!$resultT) {
+	echo "<p>ERROR: NO SE HA ELIMINADO EL TELÉFONO</p>";
+}
+else {
+	echo "<p>EL TELÉFONO HA SIDO ELIMINADO</p>";
+}
+
+?>
+<p><a href="/list.php">VOLVER AL LISTADO</a></p>
