1f4db40abd9828b03c9dbda3d8b2f04b61918656
Eduardo Pérez <eperezf@pisapapeles.net>
2016-01-29 02:28:40 -0300

    v1.0.3!

--- result.php
+++ result.php
@@ -442,7 +442,7 @@ if ($_GET["Operadora"] == ""){
 </div>
 <footer class="footer">
     <div class="container">
-      <p class="text-muted">Verificador de Bandas <a href="about.php">1.0.2</a> Copyright © <?php echo date("Y") ?> Pisapapeles Networks Ltda. </p>
+      <p class="text-muted">Verificador de Bandas <a href="about.php">1.0.3</a> Copyright © <?php echo date("Y") ?> Pisapapeles Networks Ltda. </p>
     </div>
   </footer>
 </body>
